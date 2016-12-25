5c250cd4aaa7e9ddc3b8248638a4b15a0261fe23
guofeipeng <guofeipeng@meituan.com>
2016-12-25 21:27:38 +0800

    test

--- src/main/java/com/hack/test/Test.java
+++ src/main/java/com/hack/test/Test.java
@@ -3,24 +3,6 @@ package com.hack.test;
 public class Test {
 
    public  void test(){
-      TaobaoClient client = new DefaultTaobaoClient(url, appkey, secret);
-      TbkItemGetRequest req = new TbkItemGetRequest();
-      req.setFields("num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick");
-      req.setQ("女装");
-      req.setCat("16,18");
-      req.setItemloc("杭州");
-      req.setSort("tk_rate_des");
-      req.setIsTmall(false);
-      req.setIsOverseas(false);
-      req.setStartPrice(10L);
-      req.setEndPrice(10L);
-      req.setStartTkRate(123L);
-      req.setEndTkRate(123L);
-      req.setPlatform(1L);
-      req.setPageNo(123L);
-      req.setPageSize(20L);
-      TbkItemGetResponse rsp = client.execute(req);
-      System.out.println(rsp.getBody());
 
    }
 
